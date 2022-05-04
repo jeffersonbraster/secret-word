cf91d98fbc6d1a646ef1832118bf1570d0bec168
Jefferson Brandao <jeffersonbraster@gmail.com>
2022-05-03 23:47:52 -0300

    estilizando a tela do game

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -6,10 +6,34 @@ type GameProps = {
 
 const Game = ({ verifyLetter }: GameProps) => {
   return (
-    <div>
-      <h1>game</h1>
+    <div className={styles.container}>
+      <p className={styles.points}>
+        <span>Pontuação: 000</span>
+      </p>
 
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+      <h1>Dica da palavra:</h1>
+
+      <h2>Carro</h2>
+
+      <div className={styles.wordContainer}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blankSquare}></span>
+      </div>
+
+      <div className={styles.letterContainer}>
+        <p>Advinhe uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+
+          <button type="submit">Jogar</button>
+        </form>
+      </div>
+
+      <div className={styles.wrongLettersContainer}>
+        <p>Letras já utilizadas: </p>
+        <span>a,</span>
+        <span>b</span>
+      </div>
     </div>
   );
 };
